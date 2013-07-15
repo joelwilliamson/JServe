5a271b076b8faeba9f81a8b688bad173717a4ff9
Joel Williamson <joel.s.williamson@gmail.com>
2013-07-14 23:20:15 -0400

    Added backend_manager

--- backend_manager.hpp
+++ backend_manager.hpp
@@ -2,6 +2,10 @@
 
 struct client_t;
 
+/* Scans the backend plugin directory and links to any plugins found there. This
+ * should be called from the main() function, prior to any calls being made to
+ * get_resource().
+ */
 int register_backend_plugins();
 
 /* Extract the file extension from path and use that to fetch the proper resource
